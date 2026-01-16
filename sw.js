dc83c5e0267e5a2a5563f7b4b3c434438798f42f
aalmagrodv-arch <a.almagrodv@gmail.com>
2026-01-16 03:15:07 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+// sw.js
+const CACHE_NAME = 'mi-coleccion-v3'; // Sube a v3, v4... cuando publiques cambios importantes
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './libs/xlsx.full.min.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  // OJO: NO hacemos skipWaiting aquí. Esperamos al usuario.
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      return cached || fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
